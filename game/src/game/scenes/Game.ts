236d588d7d309ac9aef505cdf8bf10b32b7636cf
Leonel935 <Leonel.galende@uap.edu.ar>
2024-05-07 02:46:47 -0300

    Variable que se actualiza

--- game/src/game/scenes/Game.ts
+++ game/src/game/scenes/Game.ts
@@ -5,7 +5,10 @@ export class Game extends Scene {
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
     occupiedPositions: { x: number, y: number }[];
-    
+    maderaText: Phaser.GameObjects.Text; // Texto para mostrar la cantidad de madera
+
+    // Variable para almacenar la cantidad de madera
+    maderaCount: number = 0;
 
     constructor() {
         super('Game');
@@ -14,28 +17,31 @@ export class Game extends Scene {
 
     create() {
         this.fondo();
+
+        // Crear el texto para mostrar la cantidad de madera
+        this.maderaText = this.add.text(this.sys.canvas.width - 50, 25, 'Madera: 0', { fontFamily: 'Arial', fontSize: '24px', color: '#ffffff' });
+        this.maderaText.setOrigin(1, 0);
     }
+
     fondo(){
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
         
-
-
         const grid = this.add.grid(512, 384, 1024, 768, 64, 64, 0xffffff);
         grid.setOrigin(0.5);
         grid.setAltFillStyle(0xcccccc);
         grid.setOutlineStyle();
         grid.setAlpha(0.5);  
-        // Agregar una imagen en cada cuadrado de la grilla para visualizar mejor las celdas de la grilla
+
         for (let i = 0; i < grid.width+100; i += 64) {
             for (let j = 0; j < grid.height+100; j += 64) {
                 this.add.image(i, j, 'pasto').setAlpha(0.5);
             }
         }
- 
     }
 
     update() {
-        
+        // Actualizar el texto de la cantidad de madera
+        this.maderaText.setText(`Madera: ${this.maderaCount}`);
     }
 }
